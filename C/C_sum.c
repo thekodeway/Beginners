5d1372b9e99d6c6b0e2272a1e34da3bd46d85517
thekodeway <61931801+thekodeway@users.noreply.github.com>
2020-10-02 10:02:11 +0530

    Used Recursion to optimise time and space complexity
    used recursion to avoid the usage of for loop and reduce the time and space complexity

--- C/C_sum.c
+++ C/C_sum.c
@@ -1,19 +1,19 @@
 
 
 #include <stdio.h>
+int sum(int num)
+{
+    if(num==0)
+    {
+        return 0;
+    }
+    return num+(num-1);
+}
 int main()
 {
-    int n, i, sum = 0;
-    
+    int n,sum = 0;
     printf("Enter a positive integer: ");
     scanf("%d",&n);
-    
-    for(i=1; i <= n; ++i)
-    {
-        sum += i;   // sum = sum+i;
-    }
-    
-    printf("Sum = %d",sum);
-    
+    printf("Sum = %d",sum(n));
     return 0;
 }
